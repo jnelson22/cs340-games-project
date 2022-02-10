929552af0146162315b72a6d19522daaab2a8d93
Jeff Nelson <jnelson22@gmail.com>
2022-02-09 23:16:42 -0800

    update app

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+from email.policy import default
 from flask import Flask, render_template, send_from_directory, request, jsonify, make_response
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_folder='games-ui/build/', static_url_path='')
+app = Flask(__name__, static_folder='games-ui/build', static_url_path='')
 cors = CORS(app)
 
-@app.route('/')
-def serve():
+@app.route('/', defaults={'path': ''})
+def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='https://cs340-games-project.herokuapp.com/')
