130b9fa3a6533ae938226fbe1959973bffc30f20
Jeff Nelson <jnelson22@gmail.com>
2022-02-09 22:50:15 -0800

    initial flask commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template,send_from_directory,request, jsonify, make_response
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__,static_folder='games-ui/build',static_url_path='')
+cors = CORS(app)
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
