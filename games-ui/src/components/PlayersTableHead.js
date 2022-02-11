761646cc5d311681d3c8f4b09f90f907e1cef876
Jeff Nelson <jnelson22@gmail.com>
2022-02-10 18:10:00 -0800

    added table header for players

---
+++ games-ui/src/components/PlayersTableHead.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PlayersTableHead() {
+    return (
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+            </tr>
+        </thead>
+    );
+          
+}
--- games-ui/src/pages/PlayersPage.js
+++ games-ui/src/pages/PlayersPage.js
@@ -5,7 +5,7 @@ function PlayersPage() {
     return (
         <>
             <h1>Players Page</h1>
-                <Link className="App-link" to="/">Go to the Home Page</Link>
+                
         </>
     );
 }
