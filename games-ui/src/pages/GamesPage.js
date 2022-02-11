eea6f685d854db158024913350e53832c3b8c51d
swartsm <margaret@10-197-149-99.sds.oregonstate.edu>
2022-02-10 19:58:03 -0800

    added games table

--- games-ui/src/pages/GamesPage.js
+++ games-ui/src/pages/GamesPage.js
@@ -4,10 +4,21 @@ import { Link } from 'react-router-dom';
 function GamesPage() {
     return (
         <>
-            <h1>Games Page</h1>
-                <Link className="App-link" to="/">Go to the Home Page</Link>
+            <h1>Games</h1>
+            <div className="gamesTable">
+                <table>
+                    <caption>Available Games</caption>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Minimum number of players</th>
+                            <th>Maximum number of players</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </>
-        
+
     );
 }
 
