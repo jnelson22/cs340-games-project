a9b65672811e75e5b1b025e281504c21374f03ea
Jeff Nelson <jnelson22@gmail.com>
2022-02-15 23:03:35 -0800

    added initial search bar

--- games-ui/src/components/PlayersTableHead.js
+++ games-ui/src/components/PlayersTableHead.js
@@ -7,6 +7,7 @@ function PlayersTableHead() {
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Favorite Game</th>
+                <th>Edit</th>
             </tr>
         </thead>
     );
--- games-ui/src/pages/PlayersPage.js
+++ games-ui/src/pages/PlayersPage.js
@@ -7,6 +7,23 @@ function PlayersPage() {
     return (
         <>
             <h1>Players Page</h1>
+            <table className="table-container">
+                <tr>
+                    <td>
+                        <input type="text" placeholder='First'/>
+                    </td>
+                    <td>
+                        <input type="text" placeholder='Last'/>
+                    </td>
+                    <td>
+                        <input type="text" placeholder='Favorite Game'/>
+                    </td>
+                    <td>
+                        <button>Search</button>
+                    </td>
+                </tr>
+            </table>
+            <br></br>
             <PlayersTable />
             <hr></hr>
             <table className="table-edit">
