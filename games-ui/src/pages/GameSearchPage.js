03ddb0f85d2be1103d79f79cc5472db7a4c18065
swartsm <margaret@Margarets-MacBook-Pro.local>
2022-02-16 19:06:21 -0800

    created GameSearchPage

---
+++ games-ui/src/pages/GameSearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function GameSearchPage() {
+    return(
+        <>
+            <h1>search for a game</h1>
+        </>
+    );
+}
+
+export default GameSearchPage;
