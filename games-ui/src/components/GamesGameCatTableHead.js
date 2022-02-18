2a7aa3c7d5bade8e3c9fbb6292f5d71aa920a25f
swartsm <margaret@Margarets-MacBook-Pro.local>
2022-02-17 21:22:25 -0800

    added GamesGameCatTableHead function

---
+++ games-ui/src/components/GamesGameCatTableHead.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function GamesGameCatTableHead() {
+    return (
+        <thead>
+            <tr>
+                <th>game id</th>
+                <th>game category id</th>
+            </tr>
+        </thead>
+    );
+          
+}
+
+export default GamesGameCatTableHead;
