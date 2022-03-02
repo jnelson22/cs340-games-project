4a8b670f46d9fb0c57df4fe227f05e3fd1fa212a
swartsm <margaret@Margarets-MacBook-Pro.local>
2022-03-01 22:01:53 -0800

    updated games page to match ui of players

--- games-ui/src/components/GamesTableHead.js
+++ games-ui/src/components/GamesTableHead.js
@@ -1,17 +1,30 @@
 import React from "react";
 
-function GamesTableHead() {
-    return (
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Min. # of Players</th>
-                <th>Max # of Players</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-    );
+function GamesTableHead({input}) {
+    if (input==='game-add'){
+        return (
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Min. # of Players</th>
+                    <th>Max # of Players</th>
+                </tr>
+            </thead>
+        );
+    }
+    else {
+        return (
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Min. # of Players</th>
+                    <th>Max # of Players</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+        );
+    }
           
 }
 
