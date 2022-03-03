cf37289082c462bca44a830e35a1f59da1e06b0f
swartsm <margaret@Margarets-MacBook-Pro.local>
2022-03-02 21:51:23 -0800

    updated games game cat page to use state

--- games-ui/src/pages/GamesGameCatPage.js
+++ games-ui/src/pages/GamesGameCatPage.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import GamesGameCatTable from '../components/GamesGameCatTable';
+import { useState, useEffect } from 'react';
 
 function GamesGameCatPage() {
+    const [gamesGameCategories, setGamesGameCategories] = useState([]);
+    const history = useHistory();
+
+    const loadGamesGameCategories = async () => {
+        const response = await fetch('/api/gamesGameCategories'); 
+        const data = await response.json();
+        setGamesGameCategories(data);
+    }
+
+    useEffect(() => {
+        loadGamesGameCategories();
+    }, []);
     return (
         <>
             <h1>Games and Game Categories Intersection Page</h1>
