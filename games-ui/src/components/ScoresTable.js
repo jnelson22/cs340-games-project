b8d32ccd55b8011bbf5deb60661156cf33f4bd41
swartsm <margaret@Margarets-MacBook-Pro.local>
2022-03-03 19:20:46 -0800

    updated scores table

--- games-ui/src/components/ScoresTable.js
+++ games-ui/src/components/ScoresTable.js
@@ -2,27 +2,22 @@ import React from 'react';
 import ScoresTableHead from './ScoresTableHead';
 import { FiEdit2, FiDelete } from 'react-icons/fi';
 
-function ScoresTable () {
+function ScoresTable ({scores}) {
     return (
         <table className="table-container">
             <ScoresTableHead />
-
-            <tr className='player-row'>
-                <td>Jeff Nelson</td>
-                <td>Catan</td>
-                <td>35</td>
-                <td><input type="checkbox" checked/></td>
-                <td><FiEdit2 /></td>
-                <td><FiDelete /></td>
-            </tr>
-            <tr className='player-row'>
-                <td>Margaret Swarts</td>
-                <td>Checkers</td>
-                <td></td>
-                <td><input type="checkbox"/></td>
-                <td><FiEdit2 /></td>
-                <td><FiDelete /></td>
-            </tr>
+            <tbody>
+                {scores.map((score) =>
+                    <tr className='player-row'>
+                        <td>{score.fullName}</td>
+                        <td>{score.game}</td>
+                        <td>{score.score}</td>
+                        <td><input type="checkbox" checked/></td>
+                        <td><FiEdit2 /></td>
+                        <td><FiDelete /></td>
+                    </tr>
+                )}
+            </tbody>
         </table>
     )
 }
