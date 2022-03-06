b98c737f78167020320c47a9f59c9b98b05a65c5
Jeff Nelson <jnelson22@gmail.com>
2022-03-06 11:55:04 -0800

    updated form to work with api

--- games-ui/src/pages/GameCategoriesPage.js
+++ games-ui/src/pages/GameCategoriesPage.js
@@ -6,6 +6,7 @@ import {useState, useEffect} from 'react';
 
 function GameCategoriesPage() {
     const [gameCategories, setGameCategories] = useState([]);
+    const [category, setGameCat] = useState('');
     const history = useHistory();
 
     const loadGameCategories = async () => {
@@ -18,6 +19,18 @@ function GameCategoriesPage() {
         loadGameCategories();
     }, []);
 
+    const addGameCat = async () => {
+        const newGameCat = {category};
+        const response = await fetch('/api/game-categories', {
+            method: 'POST',
+            body: JSON.stringify(newGameCat),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        
+    };
+
     return (
         <>
             <h1>Game Categories</h1>
@@ -28,12 +41,16 @@ function GameCategoriesPage() {
                     <tbody>
                         <tr className="input-table">
                             <td className="input-table">
-                                <input type="text" />
+                            <input 
+                                type="text" 
+                                value={category}
+                                onChange={e => setGameCat(e.target.value)}
+                            />
                             </td>
                         </tr>
                         <tr>
                             <td colSpan= "5">
-                                <button className="add-button">Add</button>
+                                <button type='submit' className="add-button" onClick={addGameCat}>Add</button>
                             </td>
                         </tr>
                     </tbody>
