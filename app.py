268c63f22867ffbda83479eb94f8374064769e68
swartsm <margaret@Margarets-MacBook-Pro.local>
2022-03-15 15:27:25 -0700

    removed gameID from query and cursor

--- app.py
+++ app.py
@@ -40,13 +40,13 @@ def delete_game(gameID):
     db.execute_query(db_connection=db_connection, query=query, query_params=(gameID,))
     return Response(status=204)
 
-@app.route('/api/edit-game', methods=["PUT"])
+@app.route('/api/edit-game', methods=["POST"])
 def edit_game():
     db_connection = db.connect_to_database()
-    if request.method == 'PUT':
+    if request.method == 'POST':
         print("update game")
-        query = "UPDATE Games SET name=%s, min_number_player=%s, max_number_player=%s WHERE gameID=%s;"
-        cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(form_data['name'], form_data['min_number_player'], form_data['max_number_player'], form_data['gameID']))
+        query = "UPDATE Games SET name=%s, min_number_player=%s, max_number_player=%s;"
+        cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(form_data['name'], form_data['min_number_player'], form_data['max_number_player']))
         return Response(status=200)
 
 
