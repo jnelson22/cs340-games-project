57b434493035ab2432b4937cf9f91930356933be
swartsm <margaret@Margarets-MacBook-Pro.local>
2022-03-16 13:07:10 -0700

    added editPlayer function

--- games-ui/src/pages/EditPlayerPage.js
+++ games-ui/src/pages/EditPlayerPage.js
@@ -3,7 +3,27 @@ import { useHistory } from "react-router-dom";
 
 export const EditPlayerPage =({playerToEdit}) => {
     
+    const history = useHistory();
+    const [first_name, setFirstName] = useState(playerToEdit.first_name);
+    const [last_name, setLastName] = useState(playerToEdit.last_name);
+    const [favorite_game, setFavoriteGame] = useState(playerToEdit.favorite_game);
 
+    const editPlayer = async () => {
+        const editedPlayer = {first_name, last_name, favorite_game, playerID};
+        const response =await fetch(`/api/games/${playerToEdit.playerID}`, {
+            method: 'PUT',
+            body: JSON.stringify(editedPlayer),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(response.status === 200) {
+            alert('successfully edited player');
+        }else {
+            alert(`failed to edit player, status code=${response.status}`);
+        }
+        history.push('/');
+    }
     return (
         <>
         </>
